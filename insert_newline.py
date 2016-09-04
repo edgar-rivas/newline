15eb8a4048f72e6c76794091a57a0e5f75d7a26a
edgar-rivas <edgar-rivas@users.noreply.github.com>
2016-09-04 15:17:17 -0700

    replace grep with regex

--- insert_newline.py
+++ insert_newline.py
@@ -1,26 +1,35 @@
 #!/usr/bin/python2.7
 import sys
 import os
+import re
 
 file_name = sys.argv[1]
 input_file = open(file_name, 'r')
-contents = input_file.read()
-end = len(contents)+1000000
-print end
-for i in range(end):
-	temp_string = contents[i:i+12]
-	if temp_string == '</Parameter>':
-		contents = contents[:i+12] + '\n' + contents[i+12:]
-output_filename = file_name[:-4]+'_new'+file_name[-4:]
-output_file = open(output_filename,'wb')
-output_file.write(contents)
+output_file = open(str(file_name[:-4]+"_new"+file_name[-4:]), 'wb')
 
-print output_filename
+contents = input_file.read()
+end = len(contents)
+i = 0
+init_strip=0
+
+while i < end:
+	temp_string = contents[i:i+11]
+	if temp_string == '<Parameter>':
+		if init_strip == 0:
+			contents = contents[i:]
+			end = len(contents)
+			init_strip = 1
+			i = 11
+			continue
+		contents = contents[:i] + '\n' + contents[i:]
+		i+=1
+		end = len(contents)
+	i+=1
+
+for line in contents.splitlines():
+	if re.search('<Parameter>',line):
+		print line
+		output_file.write(line+'\n')		
+
 input_file.close()
 output_file.close()
-
-input_file_2 = open(output_filename, 'r')
-grep_str = 'grep -i "<parameter>" '+output_filename+' > '+output_filename[:-4]+'_grep'+output_filename[-4:]
-os.system(grep_str)
-
-#print contents
